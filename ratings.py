c5e34c83cd7d735f7798f34a1a022cc692fcde19
Carina Ogbat <carinawrightogbat@gmail.com>
2022-08-18 20:55:54 -0700

    added user input function

--- ratings.py
+++ ratings.py
@@ -7,19 +7,29 @@
 
 
 
-
+restaurant_ratings = {}
 
 def create_rest_rating_dict(filename):
     file = open(filename)
-    restaurant_ratings = {}
     for line in file:
         restaurant, rating = line.rstrip().split(':')
         restaurant_ratings[restaurant] = rating
         restaurant_sorted = sorted(restaurant_ratings.keys())
         for restaurant in restaurant_sorted:
             print(f"{restaurant} rating is {restaurant_ratings[restaurant]}")
-    return ""
+    return restaurant_sorted
 
-
-   
 print(create_rest_rating_dict('scores.txt'))
+
+def ask_user_rest_and_rating(filename):
+    new_user_rest = input("Please type the name of the restaurant you want to review ")
+    new_user_rest_rating = input("Please rate the restaurant [1-5] ")
+    restaurant_ratings[new_user_rest] = new_user_rest_rating
+    sorted = create_rest_rating_dict(filename)
+    print(sorted)
+
+create_rest_rating_dict('scores.txt')
+ask_user_rest_and_rating('scores.txt')
+
+
+
